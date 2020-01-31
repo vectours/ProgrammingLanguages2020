53b3d9028083cd66c4430472b5c53b3bb32b490b
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.westminster.net>
2020-01-31 13:03:36 -0500

    fixed bug in phone exercise

--- SICP/Practice/Section 1/myFirstPractice.scm
+++ SICP/Practice/Section 1/myFirstPractice.scm
@@ -2,6 +2,6 @@
         (println "AUTHOR: Sara Ann saraabrackett@westminster.net")
     )
  (inspect (/ 999 999))
- (inspect (/(* 404 2) 2)
+ (inspect (/(* 404 2) 2))
  (inspect (+ 100 526 10 184))
  (inspect (- 8505 1111))
