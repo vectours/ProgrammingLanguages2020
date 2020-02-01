39acd5893cbef2c3b1fcd3bdafcc3bcbbc22a13e
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-02-01 16:28:57 -0500

    added file reading2notes with section 1.1.2 notes

---
+++ SICP/Practice/Section 1/reading2notes.scm
@@ -0,0 +1,11 @@
+; section 1.1.2: naming and the enviroment
+(define size 2)
+(inspect size)
+
+(define pi 3.14149)
+(inspect pi)
+(define radius 20)
+(inspect radius)
+(inspect (* pi (* radius radius)))
+
+
