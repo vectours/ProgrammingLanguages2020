9e5342debc771011220235c2ccd04aae25aa45ee
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-02-01 16:38:27 -0500

    added section 1.1.4 exercises to reading2notes.scm

--- SICP/Practice/Section 1/reading2notes.scm
+++ SICP/Practice/Section 1/reading2notes.scm
@@ -11,3 +11,16 @@
 ; section 1.1.3: evaluating combinations
 (inspect (* (+ 2 (* 4 6) (+ 3 5 7))))
 
+; section 1.1.4: compound procedures
+(define (square x) (* x x))
+(inspect (square 21))
+(inspect (square (+ 2 5)))
+(inspect (square 3))
+
+(define (sum-of-squares x y) 
+    (+ (square x) (square y)))
+(inspect (sum-of-squares 3 4))
+
+(define (f a) 
+    (sum-of-squares (+ a 1) (* a 2)))
+(inspect (f 5))
