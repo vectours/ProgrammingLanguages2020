f4554f7ac5b636e4f5465082d8320cdf6b5e94d2
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.westminster.net>
2020-02-05 15:01:30 -0500

    added more examples to introToConsCells

--- SICP/Practice/Section 1/introToConsCells.scm
+++ SICP/Practice/Section 1/introToConsCells.scm
@@ -1,2 +1,18 @@
 (define x (cons 1 (cons 3 (cons (cons 7 8) 19))))
 (inspect x)
+
+; trying to access one
+(inspect (car x))
+
+; trying to access 3
+(inspect (car (cdr x)))
+(inspect (cadr x))
+
+; trying to print 19
+(inspect (cdddr x))
+
+; trying to print 7
+(inspect (car (car (cdr (cdr x)))))
+
+; trying to print 8
+(inspect (cdr (car (cdr (cdr x)))))
