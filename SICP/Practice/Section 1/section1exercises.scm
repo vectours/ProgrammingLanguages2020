5beb3229877b4b355361fa0be854c3d6ae2cad5c
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-02-13 07:20:21 -0500

    added 1.4 with explanation

--- SICP/Practice/Section 1/section1exercises.scm
+++ SICP/Practice/Section 1/section1exercises.scm
@@ -17,3 +17,18 @@
 ;; test:
 (inspect (twolargeSumOfSquares 2 3 4))
 ; 25
+
+; exercise 1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(inspect (a-plus-abs-b 2 -1))
+; 3
+
+(inspect (a-plus-abs-b -4 -4))
+; 0
+
+(inspect (a-plus-abs-b 3 32))
+; 35
+
+; the function  a-plus-abs-b adds the value of the variable a to the absolute value of the variable b. that means that when b is negative, the function makes it positive 
