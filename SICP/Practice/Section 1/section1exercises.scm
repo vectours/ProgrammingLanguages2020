7fbb5c841047c2831e4dca9a9893e7a0199dd7d3
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-02-13 07:21:44 -0500

    standardized formatting

--- SICP/Practice/Section 1/section1exercises.scm
+++ SICP/Practice/Section 1/section1exercises.scm
@@ -3,7 +3,7 @@
     )
 
 ; exercise 1.2
-; expected answer -0.02
+; test -- expecting -0.02
 (inspect (/ (+ 5.0 4 (- 2 (- 3 (+ 6 (/ 5 4))))) (* 3 (- 6 2) (- 2 7))))
 
 ; exercise 1.3
@@ -14,21 +14,20 @@
   (cond (> b c) (sumOfSquares a b))
 	(else (sumOfSquares a c)))
 )
-;; test:
+; test -- expecting 25
 (inspect (twolargeSumOfSquares 2 3 4))
-; 25
 
 ; exercise 1.4
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
+; test -- expecting 3
 (inspect (a-plus-abs-b 2 -1))
-; 3
 
+; test -- expecting 0
 (inspect (a-plus-abs-b -4 -4))
-; 0
 
+; test -- expecting 35
 (inspect (a-plus-abs-b 3 32))
-; 35
 
 ; the function  a-plus-abs-b adds the value of the variable a to the absolute value of the variable b. that means that when b is negative, the function makes it positive 
