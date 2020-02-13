bfb44a4474e9a202733639325095b106eb79792f
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-02-13 07:14:58 -0500

    fixed exercise 1.3

--- SICP/Practice/Section 1/section1exercises.scm
+++ SICP/Practice/Section 1/section1exercises.scm
@@ -7,11 +7,13 @@
 (inspect (/ (+ 5.0 4 (- 2 (- 3 (+ 6 (/ 5 4))))) (* 3 (- 6 2) (- 2 7))))
 
 ; exercise 1.3
-(define (sumOfSquares x y) (+(* x x) (* y y)))
-(define (twolargeSumOfSquares a b c) (
-    (cond ((and (> a c) (> b c))) (sumOfSquares a b))
-    (cond (> a b) (sumOfSquares a c))
-    (else (b c))
-))
+(define (square x) (* x x))
+(define (sumOfSquares x y) (+ (square x) (square y)))
+(define (twolargeSumOfSquares a b c)
+  (cond ((not (or (> a b) (> a c))) (sumOfSquares b c))
+  (cond (> b c) (sumOfSquares a b))
+	(else (sumOfSquares a c)))
+)
+;; test:
 (inspect (twolargeSumOfSquares 2 3 4))
-
+; 25
