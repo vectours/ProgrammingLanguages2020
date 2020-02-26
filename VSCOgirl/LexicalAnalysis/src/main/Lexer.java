3c87329cea879ff0c45e133ccfc58936ab4168f6
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.westminster.net>
2020-02-26 14:01:37 -0500

    fixed vscode issues -- in class

--- VSCOgirl/LexicalAnalysis/src/main/Lexer.java
+++ VSCOgirl/LexicalAnalysis/src/main/Lexer.java
@@ -15,52 +15,71 @@ class Lexer {
         }
 
     }
-    /*
-     * lexeme lex() throws IOException { skipWhiteSpace(); char c = (char)
-     * input.read(); // can also say input.unread(c) and it will put it back if
-     * (isEndOfInput(c)) { System.out.println("end of input reached."); return new
-     * lexeme(Types.END_OF_INPUT); }
-     * 
-     * switch (c) { // single charectar tokens case '@': return new lexeme(AT); case
-     * '#': return new lexeme(HASHTAG); case '~': return new lexeme(TILDE);
-     * 
-     * case '(': return new lexeme(OPAREN); case ')': return new lexeme(CPAREN);
-     * case '{': return new lexeme(OBRACE); case '}': return new lexeme(CBRACE);
-     * case '[': return new lexeme(OBRACKET); case ']': return new lexeme(CBRACKET);
-     * 
-     * case '+': return new lexeme(PLUS); case '-': return new lexeme(MINUS); case
-     * '*': return new lexeme(TIMES); case '/': return new lexeme(DIVIDE); case '%':
-     * return new lexeme(MODULO);
-     * 
-     * case '$': return new lexeme(IF); case '&': return new lexeme(WHILE);
-     * 
-     * default: // numbers, variables & strings if (Character.isDigit(ch)) {
-     * input.pushback(ch); return lexNumber(); } else if (Character.isLetter(ch)) {
-     * input.pushback(ch); return lexVariable(); } else if (ch == '\"') { return
-     * lexString(); } else { return new Lexeme(UNKNOWN, ch); } }
-     * 
-     * }
-     * 
-     * private Lexeme lexNumber() throws IOException { var ch; String token = ""; ch
-     * = input.read(); while (isDigit(ch)) { token = token + ch; ch = input.read();
-     * } input.pushback(ch);
-     * 
-     * return new Lexeme(NUMBER, Integer.parseInt(token)); }
-     * 
-     * private Lexeme lexVariable() throws IOException { // no keywords that aren't
-     * symbols in my language var ch; String token = ""; ch = input.read(); while
-     * (isLetter(ch) || isDigit(ch)) { token = token + ch; ch = input.read(); }
-     * input.pushback(ch);
-     * 
-     * return new Lexeme(KEY, token); }
-     * 
-     * private Lexeme lexString() throws IOException { var ch; String token = ""; ch
-     * = input.read(); while (ch != '\"') { token = token + ch; ch = input.read(); }
-     * input.pushback(ch);
-     * 
-     * return new Lexeme(STRING, token); }
-     * 
-     * private void skipWhiteSpace() throws IOException { var ch1; while
-     * (isWhiteSpace(ch)) { ch = Input.read(); } Input.pushback(ch); }
-     */
+
+    lexeme lex() throws IOException { skipWhiteSpace(); char c = (char)
+     input.read(); // can also say input.unread(c) and it will put it back if
+     (isEndOfInput(c)) { System.out.println("end of input reached."); return new
+     lexeme(Types.END_OF_INPUT); }
+     
+     switch (c) { // single charectar tokens case '@': return new lexeme(AT); case
+     '#': return new lexeme(HASHTAG); case '~': return new lexeme(TILDE);
+     
+     case '(': return new lexeme(OPAREN); case ')': return new lexeme(CPAREN);
+     case '{': return new lexeme(OBRACE); case '}': return new lexeme(CBRACE);
+     case '[': return new lexeme(OBRACKET); case ']': return new lexeme(CBRACKET);
+     
+     case '+': return new lexeme(PLUS); case '-': return new lexeme(MINUS); case
+     '*': return new lexeme(TIMES); case '/': return new lexeme(DIVIDE); case '%':
+     return new lexeme(MODULO);
+     
+     case '$': return new lexeme(IF); case '&': return new lexeme(WHILE);
+     
+     default: // numbers, variables & strings if (Character.isDigit(ch)) {
+     input.pushback(ch); return lexNumber(); } else if (Character.isLetter(ch)) {
+     input.pushback(ch); return lexVariable(); } else if (ch == '\"') { return
+     lexString(); } else { return new Lexeme(UNKNOWN, ch); } }
+
+    }
+
+    private Lexeme lexNumber() throws IOException {
+        var ch;
+        String token = "";
+        ch = input.read();
+        while (isDigit(ch)) {
+            token = token + ch;
+            ch = input.read();
+        }
+        input.pushback(ch);
+
+        return new Lexeme(NUMBER, Integer.parseInt(token));
+    }
+
+    private Lexeme lexVariable() throws IOException { // no keywords that aren't
+     symbols in my language var ch; String token = ""; ch = input.read(); while
+     (isLetter(ch) || isDigit(ch)) { token = token + ch; ch = input.read(); }
+     input.pushback(ch);
+     
+     return new Lexeme(KEY, token); }
+
+    private Lexeme lexString() throws IOException {
+        var ch;
+        String token = "";
+        ch = input.read();
+        while (ch != '\"') {
+            token = token + ch;
+            ch = input.read();
+        }
+        input.pushback(ch);
+
+        return new Lexeme(STRING, token);
+    }
+
+    private void skipWhiteSpace() throws IOException {
+        var ch1;
+        while (isWhiteSpace(ch)) {
+            ch = Input.read();
+        }
+        Input.pushback(ch);
+    }
+
 }
--- VSCOgirl/LexicalAnalysis/src/main/LexerTest.java
+++ VSCOgirl/LexicalAnalysis/src/main/LexerTest.java
@@ -1,12 +1,11 @@
 
 import java.io.IOException;
-import javax.lang.model.util.Types;
 
 public class LexerTest {
-    private static final String testInputDirectory = "./VSCOgirl/LexicalAnalysis/TestInput";
+    private static final String testInputDirectory = "./TestInput";
 
     public static void main(String[] args) {
-        Lexer lexer = new Lexer(testInputDirectory + "/myTesting.txt");
+        Lexer lexer = new Lexer(testInputDirectory + "/myTest.txt");
         /*
          * try { Lexeme currentLexeme = lexer.lex(); while (currentLexeme.getTypes() !=
          * Types.END_OF_INPUT) { System.out.println(currentLexeme); currentLexeme =
