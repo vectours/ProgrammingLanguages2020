95fc269ae4602f43c7e4d7c2c19870240a9158ad
Sara Ann Brackett <saraabrackett@usmbp-2017l3072.lan>
2020-03-30 11:25:27 -0400

    added core recognizer functions

--- VSCOgirl/RecognizingAndParsing/src/main/Recognizer.java
+++ VSCOgirl/RecognizingAndParsing/src/main/Recognizer.java
@@ -1,4 +1,48 @@
 package main;
 
+import java.io.IOException;
+
 public class Recognizer {
+    private String inputfilepath;
+    private Lexer lexer;
+    private Lexeme currentLexeme;
+
+    Recognizer(String inputfilepath){
+        this.inputfilepath = inputfilepath;
+        lexer = new Lexer(inputfilepath);
+    }
+
+    void recognize(){
+        try{
+            System.out.println("Parsing" + inputfilepath + "...");
+            currentLexeme = lexer.lex();
+            //program();
+            System.out.println(inputfilepath + "is syntactically valid.");
+
+        }
+        catch(IOException e) {
+            System.out.println("Syntax error " + e);
+        }
+    }
+
+    // CORE RECOGNIZER FUNCTIONS
+    private boolean check(Types type) {
+        return currentLexeme.getType() == type;
+    }
+    private void match(Types type) throws IOException {
+        matchNoAdvance(type);
+        advance();
+    }
+    private void matchNoAdvance(Types type) throws IOException{
+        if(!(check(type))) {
+            throw new IOException("Syntax Error");
+        }
+    }
+    private void advance() throws IOException {
+        currentLexeme = lexer.lex();
+    }
+
+    // MATCHING FUNCTIONS
+
+    // PENDING FUNCTIONS
 }
--- VSCOgirl/RecognizingAndParsing/src/main/RecognizerTest.java
+++ VSCOgirl/RecognizingAndParsing/src/main/RecognizerTest.java
@@ -1,4 +1,10 @@
 package main;
 
 public class RecognizerTest {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String testFilePath = "../TestInput/scratchFile.txt";
+        Recognizer recognizer = new Recognizer(testFilePath);
+        recognizer.recognize();
+    }
 }
